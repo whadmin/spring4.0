a99508e8872d886ba650b8eb0fab24af5bb5dac0
wuhao.w <wuhao.w@alibaba-inc.com>
2018-12-21 19:08:17 +0800

    commit

--- src/main/java/com/spring/common/propertySource/SimpleCommandLinePropertySourceTests.java
+++ src/main/java/com/spring/common/propertySource/SimpleCommandLinePropertySourceTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.core.env.CommandLinePropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
+import org.springframework.core.env.StandardEnvironment;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
@@ -13,8 +14,8 @@ import static org.junit.Assert.assertThat;
 public class SimpleCommandLinePropertySourceTests {
 
     /**
-     *
-     *
+     * 测试默认不带参数的String[] args
+     * SimpleCommandLinePropertySource
      */
     @Test
     public void withDefaul() {
@@ -29,6 +30,40 @@ public class SimpleCommandLinePropertySourceTests {
         assertThat(((SimpleCommandLinePropertySource) ps).getPropertyNames().length,equalTo(0));
         assertThat(ps.containsProperty("foo"), is(false));
         assertThat(ps.getProperty("foo"), nullValue());
+    }
+
+    @Test
+    public void withOptionArgs() {
+        CommandLinePropertySource<?> ps =
+                new SimpleCommandLinePropertySource("--o1=v1", "noa1", "--o2", "noa2");
+        assertThat(ps.getPropertyNames().length, is(4));
+
+        assertThat(ps.containsProperty("o1"), is(true));
+        assertThat(ps.getProperty("o1"), equalTo("v1"));
+
+        assertThat(ps.containsProperty("o2"), is(true));
+        assertThat(ps.getProperty("o2"), equalTo(""));
+
+        assertThat(ps.containsProperty("o3"), is(false));
+        assertThat(ps.getProperty("o3"), nullValue());
+
+        assertThat(ps.containsProperty("nonOptionArgs"), is(false));
+        assertThat(ps.getProperty("nonOptionArgs"), nullValue());
+
+        assertThat(ps.containsProperty("nonOptionArgs"), is(true));
+        String nonOptionArgs = ps.getProperty("nonOptionArgs");
+        assertThat(nonOptionArgs, equalTo("noa1,noa2"));
+
+        ps.setNonOptionArgsPropertyName("NOA");
+        nonOptionArgs = ps.getProperty("NOA");
+        assertThat(nonOptionArgs, equalTo("noa1,noa2"));
+
+        StandardEnvironment env = new StandardEnvironment();
+        env.getPropertySources().addFirst(ps);
+        String[] nonOptionArgsArray = env.getProperty("nonOptionArgs", String[].class);
+        assertThat(nonOptionArgsArray[0], equalTo("noa1"));
+        assertThat(nonOptionArgsArray[1], equalTo("noa2"));
+
     }
 
 
